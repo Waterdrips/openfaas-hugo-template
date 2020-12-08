4a2731a5afd1f271c8e9c7a054ea189ec65baf30
Alistair Hey <alistair@heyal.co.uk>
2020-12-08 21:15:51 +0000

    make multiarch for armv7
    Signed-off-by: Alistair Hey <alistair@heyal.co.uk>

--- template/hugo/getflavor.sh
+++ template/hugo/getflavor.sh
@@ -4,6 +4,8 @@ if [ $1 == "linux/amd64" ] ; then
     flavor="Linux-64bit"
 elif [ $1 == "linux/arm64" ] ; then
     flavor="Linux-ARM64"
+elif [ $1 == "linux/arm/v7" ] ; then
+    flavor="Linux-ARM"
 fi
 
 echo $flavor
