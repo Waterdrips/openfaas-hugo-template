b5873e6e69d00a66c3d3e8941e3368fb89c88d38
Alistair Hey <alistair@heyal.co.uk>
2020-12-09 16:04:01 +0000

    Add theme
    Signed-off-by: Alistair Hey <alistair@heyal.co.uk>

--- template/hugo/getflavor.sh
+++ template/hugo/getflavor.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 export flavor="Linux-64bit"
-if [ $1 == "linux/amd64" ] ; then
+if [ "$1" == "linux/amd64" ] ; then
     flavor="Linux-64bit"
-elif [ $1 == "linux/arm64" ] ; then
+elif [ "$1" == "linux/arm64" ] ; then
     flavor="Linux-ARM64"
-elif [ $1 == "linux/arm/v7" ] ; then
+elif [ "$1" == "linux/arm/v7" ] ; then
     flavor="Linux-ARM"
 fi
 
